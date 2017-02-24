025004afa036df465817b8427e656dc80c27eb2a
mittya <mittya@foxmail.com>
2017-02-24 16:49:56 +0800

    Fix downloads
    Instagram support add more images and videos at one time

--- chrome/script.js
+++ chrome/script.js
@@ -1,77 +1,60 @@
 if (window.location.pathname === '/') {
-
-  // 首页信息流页
-
+  //  Home page
   var _box = document.querySelector('._qj7yb');
-  var _parent, _url;
 
-  // 已登录
+  // Logged in
   if (_box) {
-    _box.addEventListener('mouseover', function(event) {
-
-      if (event.target.className === '_icyx7') {
-        _parent = event.target.parentNode;
-        _url = event.target.src;
-
-        addBtn(_parent, _url);
-      }
-
-      if (event.target.className === '_c2kdw') {
-        _parent = event.target.parentNode;
-        _url = _parent.querySelector('._c8hkj').src;
-
-        addBtn(_parent, _url);
-      }
-
-    });
+    findMedia(_box);
   }
-
 } else if (window.location.pathname.match('/p/')) {
-
-  // 详细页
-
+  // Detail page
   var _box = document.querySelector('article._j5hrx');
+
+  findMedia(_box);
+}
+
+
+function findMedia(box) {
+  var _box = box;
   var _parent, _url;
 
-  if (_box.querySelector('._jjzlb')) {
-    _parent = _box.querySelector('._jjzlb');
-    _url = _parent.querySelector('._icyx7').src;
-  }
+  _box.addEventListener('mouseover', function(event) {
+    if (event.target.className === '_icyx7') {
+      _parent = event.target.parentNode;
+      _url = event.target.src;
 
-  if (_box.querySelector('._q3gpi')) {
-    _parent = _box.querySelector('._q3gpi');
-    _url = _parent.querySelector('._c8hkj').src;
-  }
+      addBtn(_parent, _url);
+    }
 
-  addBtn(_parent, _url);
+    if (event.target.className === '_c2kdw') {
+      _parent = event.target.parentNode;
+      _url = _parent.querySelector('._c8hkj').src;
 
-} else {
-
-  // 用户页
-
+      addBtn(_parent, _url);
+    }
+  });
 }
 
 function addBtn(parent, url) {
+  var _parent = parent;
+  var _url = url;
 
-  if (!parent.querySelector('.downloadBtn')) {
-    var _parent = parent;
-    var _url = url;
-    var _btn = document.createElement('button');
-
-    _btn.className = 'downloadBtn';
-
-    _btn.addEventListener('click', function(e) {
-      e.stopPropagation();
-
-      // 下载
-      chrome.runtime.sendMessage({
-        msg: 'DL',
-        url: _url
-      });
-
-    }, false);
-
-    _parent.appendChild(_btn);
+  if (_parent.querySelector('.downloadBtn')) {
+    _parent.removeChild(_parent.querySelector('.downloadBtn'));
   }
 
+  var _btn = document.createElement('button');
+  _btn.className = 'downloadBtn';
+
+  _btn.addEventListener('click', function(e) {
+    e.stopPropagation();
+
+    // Download
+    chrome.runtime.sendMessage({
+      msg: 'DL',
+      url: url
+    });
+  }, false);
+
+  _parent.appendChild(_btn);
 }
