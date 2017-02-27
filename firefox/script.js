9b2f48be4a021b9eb9c67d7f0da53bb3c203dd38
mittya <mittya@foxmail.com>
2017-02-27 17:58:06 +0800

    Update firefox version

--- firefox/script.js
+++ firefox/script.js
@@ -1,77 +1,89 @@
+Element.prototype.parents = function(selector) {
+  // Vanilla JS jQuery.parents() realisation
+  // https://gist.github.com/ziggi/2f15832b57398649ee9b
+
+  var elements = [];
+  var elem = this;
+  var ishaveselector = selector !== undefined;
+
+  while ((elem = elem.parentElement) !== null) {
+    if (elem.nodeType !== Node.ELEMENT_NODE) {
+      continue;
+    }
+
+    if (!ishaveselector || elem.matches(selector)) {
+      elements.push(elem);
+    }
+  }
+
+  return elements;
+};
+
+
+/*
+  Main
+*/
 if (window.location.pathname === '/') {
-
-  // 首页信息流页
-
+  //  Home page
   var _box = document.querySelector('._qj7yb');
-  var _parent, _url;
 
-  // 已登录
+  // Logged in
   if (_box) {
-    _box.addEventListener('mouseover', function(event) {
-
-      if (event.target.className === '_icyx7') {
-        _parent = event.target.parentNode;
-        _url = event.target.src;
-
-        addBtn(_parent, _url);
-      }
-
-      if (event.target.className === '_c2kdw') {
-        _parent = event.target.parentNode;
-        _url = _parent.querySelector('._c8hkj').src;
-
-        addBtn(_parent, _url);
-      }
-
-    });
+    findMedia(_box);
   }
-
 } else if (window.location.pathname.match('/p/')) {
-
-  // 详细页
-
+  // Detail page
   var _box = document.querySelector('article._j5hrx');
-  var _parent, _url;
 
-  if (_box.querySelector('._jjzlb')) {
-    _parent = _box.querySelector('._jjzlb');
-    _url = _parent.querySelector('._icyx7').src;
-  }
-
-  if (_box.querySelector('._q3gpi')) {
-    _parent = _box.querySelector('._q3gpi');
-    _url = _parent.querySelector('._c8hkj').src;
-  }
-
-  addBtn(_parent, _url);
-
-} else {
-
-  // 用户页
-
+  findMedia(_box);
 }
 
-function addBtn(parent, url) {
 
-  if (!parent.querySelector('.downloadBtn')) {
-    var _parent = parent;
-    var _url = url;
-    var _btn = document.createElement('button');
+function findMedia(box) {
+  var _box = box;
+  var _parent, _url, _username;
 
-    _btn.className = 'downloadBtn';
+  _box.addEventListener('mouseover', function(event) {
+    if (event.target.className === '_icyx7') {
+      _parent = event.target.parentNode;
+      _url = event.target.src;
+      _username = _parent.parents('article._j5hrx')[0].querySelector('._4zhc5').title;
 
-    _btn.addEventListener('click', function(e) {
-      e.stopPropagation();
+      addBtn(_parent, _url, _username);
+    }
 
-      // 下载
-      chrome.runtime.sendMessage({
-        msg: 'DL',
-        url: _url
-      });
+    if (event.target.className === '_c2kdw') {
+      _parent = event.target.parentNode;
+      _url = _parent.querySelector('._c8hkj').src;
+      _username = _parent.parents('article._j5hrx')[0].querySelector('._4zhc5').title;
 
-    }, false);
+      addBtn(_parent, _url, _username);
+    }
+  });
+}
 
-    _parent.appendChild(_btn);
+function addBtn(parent, url, username) {
+  var _parent = parent;
+  var _url = url;
+  var _filename = username + '_' + _url.substring(_url.lastIndexOf('/') + 1, _url.length);
+
+  if (_parent.querySelector('.downloadBtn')) {
+    _parent.removeChild(_parent.querySelector('.downloadBtn'));
   }
 
+  var _btn = document.createElement('button');
+  _btn.className = 'downloadBtn';
+
+  _btn.addEventListener('click', function(event) {
+    event.stopPropagation();
+
+    // Download
+    chrome.runtime.sendMessage({
+      msg: 'DL',
+      url: _url,
+      filename: _filename
+    });
+  }, false);
+
+  _parent.appendChild(_btn);
 }
