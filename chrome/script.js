9767cae585c43352ff686bdfc95a35edfdac13a8
mittya <mittya@foxmail.com>
2022-06-04 17:45:47 +0800

    Fix picture download #61

--- chrome/script.js
+++ chrome/script.js
@@ -28,14 +28,14 @@ Element.prototype.parents = function(selector) {
 */
 /*  Home page */
 if (window.location.pathname === '/') {
-  var _box_home = document.querySelector('#react-root section > main > section');
+  var _box_home = document.querySelector('body section > main > section');
 
   if (_box_home) {
     findMedia(_box_home);
   }
 
   setTimeout(function() {
-    _box_home = document.querySelector('#react-root section > main > section');
+    _box_home = document.querySelector('body section > main > section');
 
     if (_box_home) {
       findMedia(_box_home);
@@ -50,10 +50,10 @@ if (window.location.pathname.match('/p/') || window.location.pathname.match('/tv
   /*
     Absolute
 
-    box class: QBXjJ
+    box class: _aa6a
   */
-  if (document.querySelector('article.QBXjJ')) {
-    _box_detail = document.querySelector('article.QBXjJ');
+  if (document.querySelector('article._aa6a')) {
+    _box_detail = document.querySelector('article._aa6a');
     findMedia(_box_detail);
   }
   /*
@@ -84,7 +84,7 @@ if (window.location.pathname.match('/p/') || window.location.pathname.match('/tv
 /*  Stories page */
 if (window.location.pathname.match('/stories/')) {
   setTimeout(function() {
-    var _box_story = document.querySelector('#react-root section > div.Cd8X1');
+    var _box_story = document.querySelector('section._ac0a > div._ac0b');
 
     if (_box_story) {
       findMedia(_box_story, 'stories');
@@ -102,9 +102,9 @@ function findMedia(box, way) {
     /*
       Picture
 
-      img class: FFVAD
+      img class: _aagt
     */
-    if (event.target.className === 'FFVAD') {
+    if (event.target.className === '_aagt') {
 
       // disabled on the thumbnail page
       if (event.target.width > 300) {
@@ -112,9 +112,9 @@ function findMedia(box, way) {
         _url = event.target.src;
         _username = '';
 
-        // title class: sqdOP
-        if (_parent.parents('article')[0].querySelector('a.sqdOP')) {
-          _username = _parent.parents('article')[0].querySelector('a.sqdOP').text;
+        // title class: _aap8
+        if (_parent.parents('article')[0].querySelector('span._aap8 a')) {
+          _username = _parent.parents('article')[0].querySelector('span._aap8 a').text;
         }
 
         addBtn(_parent, _url, _username);
@@ -123,55 +123,37 @@ function findMedia(box, way) {
     }
 
     /*
-      Video
+      Video & IG TV
 
-      video class: tWeCl / Q9bIO
-      video play button class: fXIG0
+      video class: _ab1d
+      video play cover class: _aakl
     */
-    if (event.target.className.indexOf('fXIG0') >= 0) {
+    if (event.target.className === '_aakl') {
+
       _parent = event.target.parentNode;
-      _url = _parent.querySelector('.Q9bIO') ? _parent.querySelector('.Q9bIO').src
-                                             : _parent.querySelector('.tWeCl').src;
+      _url = _parent.querySelector('._ab1d').src;
       _username = '';
 
-      // title class: sqdOP
-      if (_parent.parents('article')[0].querySelector('a.sqdOP')) {
-        _username = _parent.parents('article')[0].querySelector('a.sqdOP').text;
+      // title class: _aap8
+      if (_parent.parents('article')[0].querySelector('span._aap8 a')) {
+        _username = _parent.parents('article')[0].querySelector('span._aap8 a').text;
       }
 
       addBtn(_parent, _url, _username);
-    }
 
-    /*
-      IG TV
-    */
-    if (event.target.className.indexOf('tWeCl') >= 0 || event.target.className.indexOf('Q9bIO') >= 0) {
-      _parent = event.target.parentNode;
-      _url = _parent.querySelector('.Q9bIO') ? _parent.querySelector('.Q9bIO').src
-                                             : _parent.querySelector('.tWeCl').src;
-      _username = '';
-
-      // title class: sqdOP
-      if (_parent.parents('article')[0].querySelector('a.sqdOP')) {
-        _username = _parent.parents('article')[0].querySelector('a.sqdOP').text;
-      }
-
-      addBtn(_parent, _url, _username);
     }
 
     /*
       Stories Picture & Video
 
-      szopg: #react-root > div > div > section.szopg
-      B20bj parent: cover box (when autoplay videos disable, user click the cover box to play the video)
+      _ac0y parent: cover box (when autoplay videos disable, user click the cover box to play the video)
 
       Debug: click more button stop auto video
     */
-    if (event.target.className.indexOf('B20bj') >= 0 && _way === 'stories') {
+    if (event.target.className === '_ac0y' && _way === 'stories') {
 
-      var _current_target = document.querySelector('.B20bj').parentNode;
-      _parent = _current_target.parents('.qbCDp')[0];
-      _username = _parent.parents('section')[0].querySelector('._1PU_r').title;
+      var _parent = document.querySelector('._ac0y').parents('._ac0b')[0];
+      _username = _parent.querySelector('._ac0q').querySelector('a').text;
 
       // Stories Video: video 'if' in front of the image
       if (_parent.querySelector('video')) {
@@ -184,6 +166,7 @@ function findMedia(box, way) {
 
       // Stories Picture
       if (_parent.querySelector('img')) {
+
         var _tmp = _parent.querySelector('img').srcset.split(',')[0];
         _tmp.substring(0, _tmp.indexOf(' '));
         _url = _tmp.substring(0, _tmp.indexOf(' '));
@@ -199,8 +182,6 @@ function findMedia(box, way) {
 }
 
 function addBtn(parent, url, username) {
-  /* Bad Code */
-
   var _parent = parent;
   var _url = url;
   var _url_param = url.indexOf('?') >= 0 ? url.substring(0, url.indexOf('?')) : url;
@@ -221,6 +202,9 @@ function addBtn(parent, url, username) {
 
   // Video & No Button
   if (_url.indexOf('blob') >= 0 && _flag) {
+    console.warn('Media Helper: Sorry, This version cannot be dowonload video.');
+    return false;
+
     getBlobVideo(_parent, function(oUrl) {
       _url = oUrl;
 
@@ -257,19 +241,19 @@ function addBtn(parent, url, username) {
   }, false);
 }
 
-
 function getBlobVideo(nodeParent, callback) {
   var _parent = nodeParent;
-  var _poster = _parent.querySelector('video').getAttribute('poster').match(/\/([^\/?]*)\?/)[1];
-  var _wrap = nodeParent.parents('article')[0];
+  var _wrap = _parent.parents('article')[0];
   var _times = _wrap.querySelectorAll('time');
-  var _url = _times[_times.length - 1].parentNode.href;
+  var _url = _times[_times.length - 1].parents('a')[0].href;
   var regx = /\"video_versions\":\[.*?\]/g;
 
-  fetch(_url).then(response => response.text()).then(restext => {
-    let objStr = '{' + restext.match(regx)[0] + '}';
-    let obj = JSON.parse(objStr);
+  // TODO: This version cannot be dowonload video.
 
-    callback(obj.video_versions[0].url.replace(/\\u0026/g, '&'));
-  });
+  // fetch(_url).then(response => response.text()).then(restext => {
+  //   let objStr = '{' + restext.match(regx)[0] + '}';
+  //   let obj = JSON.parse(objStr);
+
+  //   callback(obj.video_versions[0].url.replace(/\\u0026/g, '&'));
+  // });
 }
